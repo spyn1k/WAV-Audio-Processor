9649ded3e1b85c5f8b12bf97da60ad924e7fb287
spyn1k <spynik15@gmail.com>
2025-12-01 23:48:09 +0200

    Βασικός Σκελετος και arguments

--- soundwave/src/soundwave.c
+++ soundwave/src/soundwave.c
@@ -1,1 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/*ΣΚΕΛΕΤΟΣ*/
+int main(int argc, char **argv) 
+{
+    if( argv < 2)
+    {
+        fprintf(stderr, "Usage %s command", argv[0]);
+        return 1;
+    }
+
+    if(strcmp(argv[1], "info") == 0) 
+    {
+        //υλοποιηση
+    }
+        else if (strcmp(argv[1], "rate") == 0) 
+        {
+            //υλοποιηση
+        }
+            else if(strcmp(argv[1], "channel") ==0)
+            {
+                //υλοποιηση
+            }
+    else
+    {
+        fprint(stderr, "Unknown command\n");
+        return 1;
+    }
+    return 0;
+}
+
